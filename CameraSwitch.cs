48a715dd519f558bcd5c5e65644b6708bf086eb0
pilot12138 <632913013@qq.com>
2020-11-04 22:00:25 +0800

    Create CameraSwitch.cs

---
+++ CameraSwitch.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public List<GameObject> cams;           // 相机数组
+    private int camFlag;
+
+    private void Start()
+    {
+        camFlag = 1;
+        _switch();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))    // 每按下C键
+        {
+            camFlag++;                      // 标志位加一
+
+            if (camFlag > cams.Count)       // 越位归位
+            {
+                camFlag = 1;
+            }
+
+            _switch();
+        }
+    }
+
+    void _switch()  // 切换相机
+    {
+        for (int i = 0; i < cams.Count; i++)
+        {
+            if (i == camFlag - 1)
+            {
+                cams[i].SetActive(true);
+            }
+            else
+            {
+                cams[i].SetActive(false);
+            }
+        }
+    }
+}
