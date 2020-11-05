d57e82d96813ecfb16bae266684cd14d2b3a6439
pilot12138 <632913013@qq.com>
2020-11-05 09:56:26 +0800

    Update CameraSwitch.cs

--- CameraSwitch.cs
+++ CameraSwitch.cs
@@ -1,4 +1,12 @@
-﻿using System.Collections;
+﻿/*
+ * 该脚本应挂载于一个空物体下
+ * 
+ * 实现：
+ * 1.动态传入场景中多个相机
+ * 2.按C键循环切换机位
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
